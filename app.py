046193494fa2ddc32e0eb20d1a77b5a3bdff1684
rawaljatin03@gmail.com <jatinrawal99999@gmail.com>
2026-04-29 21:35:49 +0530

    flask redeploy

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-print("Webhook test working")
-print("Hello Jatin, this is your first Jenkins pipeline")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, our Flask app is running!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
